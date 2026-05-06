df755be5d007b892fe24ba3d4b1c7bc75337d602
panut kitwannee <panut.kit@itmx.co.th>
2026-05-06 14:41:43 +0700

    docker compose and setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Task Management API')
+    .setDescription('The Task API description')
+    .setVersion('1.0')
+    // .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  // for setup url (localhost:3000/docs) and documentFactory
+  SwaggerModule.setup('docs', app, documentFactory);
   await app.listen(3000);
 }
 bootstrap();
